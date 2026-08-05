bfd914d54a9e3fd9703a355f5b897126e1f8ac61
Dimas Aditya Ramadhan <dimas.adityaramadhan071104@gmail.com>
2026-08-05 21:23:52 +0700

    sikit aja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import { Suspense } from "react";
 import Navbar from "@/components/layout/navbar";
 import Hero from "@/app/dashboard/hero";
 import Categories from "@/app/dashboard/categories";
 import Galeri from "@/app/dashboard/galery";
 import Testimoni from "@/app/dashboard/testimoni";
 import Footer from "@/components/layout/footer";
+import GoogleLoginHandler from "@/components/auth/GoogleLoginHandler";
 
 export default function Home() {
   return (
     <div>
+      <Suspense fallback={null}>
+        <GoogleLoginHandler />
+      </Suspense>
       <Navbar />
       <Hero />
       <Categories />
